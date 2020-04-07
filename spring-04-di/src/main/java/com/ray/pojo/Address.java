24d9ca022a2f7f6a44c442f85a1755075ceeeb1c
Raywcl <2728928100@qq.com>
2020-04-07 23:52:56 +0800

    依赖注入，各种类型属性注入 （1）普通类型注入

---
+++ spring-04-di/src/main/java/com/ray/pojo/Address.java
@@ -0,0 +1,16 @@
+package com.ray.pojo;
+
+/**
+ * Created by Administrator on 2020/4/7.
+ */
+public class Address {
+    String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ spring-04-di/src/test/java/MyTest.java
@@ -0,0 +1,16 @@
+import com.ray.pojo.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Administrator on 2020/4/7.
+ */
+public class MyTest {
+    public static void main(String[] args) {
+
+        ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student.getName());
+
+    }
+}
