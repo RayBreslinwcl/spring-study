0e8dd5b8b3bd092be439ff0471b51c683ea12758
Raywcl <2728928100@qq.com>
2020-04-11 17:41:39 +0800

    事务问题模拟复现

--- spring-11-transaction/src/main/java/com/ray/mapper/UserMapper.java
+++ spring-11-transaction/src/main/java/com/ray/mapper/UserMapper.java
@@ -11,4 +11,9 @@ public interface UserMapper {
     //查询所有用户
     public List<User> select();
 
+    //添加
+    public int addUser(User user);
+
+    //删除
+    public int deleteUser(int id);
 }
--- spring-11-transaction/src/main/java/com/ray/mapper/UserMapperImp2.java
+++ spring-11-transaction/src/main/java/com/ray/mapper/UserMapperImp2.java
@@ -14,9 +14,25 @@ public class UserMapperImp2 extends SqlSessionDaoSupport implements UserMapper {
 //        UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
 //        return userMapper.select();
 
+        //为了测试事务，直接添加
+        User user=new User(10,"上","12453234");
+        UserMapper userMapper=getSqlSession().getMapper(UserMapper.class);
+        userMapper.addUser(user);
+        userMapper.deleteUser(6);
+
 //        精简一行
         return getSqlSession().getMapper(UserMapper.class).select();
 
+
+    }
+
+
+    public int addUser(User user) {
+        return getSqlSession().getMapper(UserMapper.class).addUser(user);
+    }
+
+    public int deleteUser(int id) {
+        return getSqlSession().getMapper(UserMapper.class).deleteUser(id);
 
     }
 }
